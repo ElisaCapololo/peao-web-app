925b55a8b9d45d1c2b93f6d8d9dbc3ce162de4c3
Elisa Capololo <elisacapololo2017@gmail.com>
2023-01-16 21:19:17 +0000

    first push with all UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/auth/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsListingComponent } from './components/marketplace/products-listing/products-listing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'products-listing', component: ProductsListingComponent},
+  {path: 'login', component: LoginComponent},
+  
 
-@NgModule({
+];
+
+@NgModule({ 
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
