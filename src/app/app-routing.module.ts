c6a361ac357f9d4ed1262381d5d0f411f8a07e18
Elisa Capololo <elisacapololo2017@gmail.com>
2023-01-21 14:48:32 +0000

    elimination of unused files for better organization of the project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './components/auth/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProductsDetailComponent } from './components/marketplace/products-detail/products-detail.component';
 import { ProductsListingComponent } from './components/marketplace/products-listing/products-listing.component';
